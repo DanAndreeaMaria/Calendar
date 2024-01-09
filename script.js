9c6518f24ae710f94db5c06e66e2629315da49ab
Andreea <106435918+DanAndreeaMaria@users.noreply.github.com>
2024-01-09 16:09:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function myFunction1() {
+    alert("Let's gooo!\nDay 1: \nEarned a new badge in HTML course!");
+}
+
+function myFunction2() {
+    alert("Day 2: \nI made a 30 min Data Science tutorial from Youtube!");
+}
+
+function myFunction3() {
+    alert("Day 3: \nCreated the final project for the HTML course, a Restaurant app.");
+}
+
+function myFunction4() {
+    alert("Day 4: \nEarned a new badge on the CSS course!");
+}
+
+function myFunction5() {
+    alert("Day 5: \nI made a simple Calculator app!");
+}
+
+function myFunction6() {
+    alert("Day 6: \nTried to update my CV - not so well done, but it was something!");
+}
+
+function myFunction7() {
+    alert("Day 7: \nCreated a To Do List app where you can check, add and delete tasks!");
+}
+
+function myFunction8() {
+    alert("Day 8: \nContinuing the CSS course!");
+}
+
+function myFunction9() {
+    alert("Day 9: \nI made this simple Calendar with onClick events!");
+}
+
+function myFunction10() {
+    alert("Coming soon...");
+}
